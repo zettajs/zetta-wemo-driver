274ee7e4dce7f9ea0db9c3ece4fc16a73cc2d580
Adam Magaluk <AdamMagaluK@gmail.com>
2014-09-30 14:21:49 -0400

    Removed cb from example

--- examples/server.js
+++ examples/server.js
@@ -2,11 +2,5 @@ var zetta = require('zetta');
 var Wemo = require('../index');
 
 zetta()
-  .expose('*')
   .use(Wemo)
-  .listen(3000, function(err) {
-    if(err) {
-      console.log(err);
-    }
-    console.log('Listening on http://localhost:3000/');
-  });
+  .listen(1337);
